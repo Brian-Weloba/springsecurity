03ac048de287f74c23783bf74f89cd0f7a6edc55
Brian-Weloba <bweloba@gmail.com>
2022-01-03 22:26:58 +0300

    Permission based authenticati

--- src/main/java/com/saturdev/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/saturdev/springsecurity/security/ApplicationSecurityConfig.java
@@ -19,54 +19,58 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final PasswordEncoder passwordEncoder;
+        private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
+        @Autowired
+        public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+                this.passwordEncoder = passwordEncoder;
+        }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
-                .antMatchers("/api/**").hasRole(STUDENT.name())
-                .antMatchers(HttpMethod.DELETE,"management/api/**").hasAuthority(COURSES_WRITE.name())
-                .antMatchers(HttpMethod.POST,"management/api/**").hasAuthority(COURSES_WRITE.name())
-                .antMatchers(HttpMethod.PUT,"management/api/**").hasAuthority(COURSES_WRITE.name())
-                .antMatchers(HttpMethod.GET,"management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
-    }
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+                http
+                                .csrf().disable()
+                                .authorizeRequests()
+                                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                                .antMatchers("/api/**").hasRole(STUDENT.name())
+                                .antMatchers(HttpMethod.DELETE, "management/api/**").hasAuthority(COURSES_WRITE.name())
+                                .antMatchers(HttpMethod.POST, "management/api/**").hasAuthority(COURSES_WRITE.name())
+                                .antMatchers(HttpMethod.PUT, "management/api/**").hasAuthority(COURSES_WRITE.name())
+                                .antMatchers(HttpMethod.GET, "management/api/**")
+                                .hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
+                                .anyRequest()
+                                .authenticated()
+                                .and()
+                                .httpBasic();
+        }
 
-    @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails peterparker = User.builder()
-                .username("peterparker")
-                .password(passwordEncoder.encode("password"))
-                .roles(ApplicationUserRole.STUDENT.name())
-                .build();
+        @Override
+        @Bean
+        protected UserDetailsService userDetailsService() {
+                UserDetails peterparker = User.builder()
+                                .username("peterparker")
+                                .password(passwordEncoder.encode("password"))
+                                // .roles(STUDENT.name())//ROLE_STUDENT
+                                .authorities(STUDENT.getGrantedAuthorities())
+                                .build();
 
-        UserDetails james = User.builder()
-                .username("james")
-                .password(passwordEncoder.encode("password"))
-                .roles(ApplicationUserRole.ADMIN.name())
-                .build();
+                UserDetails james = User.builder()
+                                .username("james")
+                                .password(passwordEncoder.encode("password"))
+                                // .roles(ADMIN.name())//ROLE_ADMIN
+                                .authorities(ADMIN.getGrantedAuthorities())
+                                .build();
 
-        UserDetails tom = User.builder()
-                .username("tom")
-                .password(passwordEncoder.encode("password"))
-                .roles(ApplicationUserRole.ADMIN_TRAINEE.name())
-                .build();
+                UserDetails tom = User.builder()
+                                .username("tom")
+                                .password(passwordEncoder.encode("password"))
+                                // .roles(ADMIN_TRAINEE.name())//ROLE_ADMIN_TRAINEE
+                                .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
+                                .build();
 
-        return new InMemoryUserDetailsManager(
-                peterparker,
-                james,
-                tom);
-    }
+                return new InMemoryUserDetailsManager(
+                                peterparker,
+                                james,
+                                tom);
+        }
 }
--- src/main/java/com/saturdev/springsecurity/security/ApplicationUserRole.java
+++ src/main/java/com/saturdev/springsecurity/security/ApplicationUserRole.java
@@ -1,8 +1,16 @@
 package com.saturdev.springsecurity.security;
 
+import java.security.Permission;
+import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.databind.module.SimpleAbstractTypeResolver;
 import com.google.common.collect.Sets;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 import static com.saturdev.springsecurity.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
@@ -14,5 +22,17 @@ public enum ApplicationUserRole {
 
     ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
         this.permissions = permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
+    }
+
+    private Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
     }
 }
